999a6e13cbe682888a5ae43a41d0c2c8536cf133
Giorgos <gkokorogiannis99@gmail.com>
2021-09-21 12:31:21 +0000

    Fixed .js problems

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function compute()
     if (principal <= 0) {
         alert("Enter a positive number")
         document.getElementById("principal").focus();
+        return;
     }
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
@@ -11,8 +12,8 @@ function compute()
     var year = new Date().getFullYear() + parseInt(years);
     
     document.getElementById("result").innerText = "If you deposit "
-    +principal+",\<br\>at an interest rate of "+rate+"%.\<br\>You will receive an amount of "
-    +interest+",\<br\>in the year "+year+"\<br\>"
+    +principal+",\nat an interest rate of "+rate+"%.\nYou will receive an amount of "
+    +interest+",\nin the year "+year+"\n"
 }
 
 function updateRate(){
